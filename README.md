895b760d2307d014f48d0e9918f59477faf44238
taqnar <muzammilthottathil.official@gmail.com>
2020-07-15 19:22:17 +0530

    completed day-2

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 <a href="https://github.com/taqnar/Coding-Challenge/tree/master/day-1">
 <img src="./img/Problem-1.svg"></a>
 
-<img src="./img/Problem-2.svg">
+<a href="https://github.com/taqnar/Coding-Challenge/tree/master/day-2">
+<img src="./img/Problem-2.svg"></a>
+
+<img src="./img/Problem-3.svg">
---
+++ day-2/k-most-frequent-number/k-most-frequent-number.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    /* ==============
+        INPUT FORMAT
+       ============== 
+       
+       first-line : number of elements of in the array (n)
+       second-line : arrray (ar) elements space seperated
+       third-line : frequency (k)
+       
+       */
+
+    int n ,k;
+
+    // Reading number of elements
+    cin >> n;
+
+    // Reading the elements of the array
+     int *ar = new int[n];   
+     for(int i=0;i<n:i++){
+         cin >> arr[i];
+     }
+
+
+    return 0;
+}
