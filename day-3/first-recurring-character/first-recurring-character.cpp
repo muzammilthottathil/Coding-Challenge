71474174190f91d9abc8a22c5170808b9cdd331b
taqnar <muzammilthottathil.official@gmail.com>
2020-07-16 20:15:32 +0530

    day-3

---
+++ day-3/first-recurring-character/first-recurring-character.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string name;
+    cin >> name;
+
+    char repeated_string;
+    unordered_map<char, int> count;
+    for (int i = 0; name[i] != '\0'; i++)
+    {
+        if (count[name[i]] > 0)
+        {
+            repeated_string = name[i];
+            break;
+        }
+        count[name[i]]++;
+    }
+
+    cout << repeated_string << endl;
+
+    return 0;
+}
